cbfbf5e4de69e383b41bbd0cb4351658814c4ebe
Bilal <bilalm354@gmail.com>
2019-11-03 21:47:09 +0000

    third commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,37 @@
 'use strict'
 var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass = require('gulp-sass');
+var useref = require('gulp-useref');
+var uglify = require('gulp-uglify');
+var gulpIf = require('gulp-if');
+var del = require('del');
 
-// Hello World
-gulp.task('hello', function(done) {
-    console.log('hello world');
-    done();
+// useref 
+gulp.task('useref', function(){
+  return gulp.src('app/*.html')
+    .pipe(useref())
+    .pipe(gulp.dest('dist'))
+    .pipe(gulpIf('*.js', uglify()))
+    .pipe(gulp.dest('dist'))
 });
 
-//  require gulp-sass plugin 
-var sass = require('gulp-sass');
-sass.compiler = require('node-sass');
+// Watch Function 
+gulp.task('watch', function(){
+  gulp.watch('app/scss/**/*.scss', gulp.series('sass')); 
+})
 
+//  require gulp-sass plugin 
+sass.compiler = require('node-sass');
 
 // SSCC to CSS and minify
 gulp.task('sass', function(){
     return gulp.src('app/scss/**/*.scss')
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
     .pipe(gulp.dest('app/css'))
-
 });
 
-gulp.task('watch', function(){
-  gulp.watch('app/scss/**/*.scss', gulp.series('sass')); 
+//del
+gulp.task('clean:dist', function() {
+  return del.sync('dist');
 })
-
-//  Minify Javascript 
-const minify = require('gulp-minify');
-
-gulp.task('compress', function(done) {
-  gulp.src(['lib/*.js', 'lib/*.mjs'])
-    .pipe(minify())
-    .pipe(gulp.dest('dist'))
-    done();
-});
